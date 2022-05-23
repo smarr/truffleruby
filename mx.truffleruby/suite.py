8663bb166211ecd3338d5946c97da59b6655c965
Stefan Marr <git@stefan-marr.de>
2022-05-23 22:17:13 +0100

    Update Graal to support Truffle hosted inlining
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -7,7 +7,7 @@ suite = {
             {
                 "name": "regex",
                 "subdir": True,
-                "version": "2d135e6767954753b05b03f50adc64c282ce15bd",
+                "version": "2ba9abb0e997db2081f02c0490b066c755190bfe",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "sulong",
                 "subdir": True,
-                "version": "2d135e6767954753b05b03f50adc64c282ce15bd",
+                "version": "2ba9abb0e997db2081f02c0490b066c755190bfe",
                 "urls": [
                     {"url": "https://github.com/oracle/graal.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
